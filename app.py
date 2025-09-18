2a12a924564c7e653bd20fc3a5c3fdcc44dd925a
Edamas <e_damas@hotmail.com>
2025-09-18 17:33:51 -0300

    fix historico de commits git

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import os # Adicionado para manipulação de caminhos
 
 # -----------------------------
 # Configuração da página
@@ -17,16 +18,18 @@ def main():
         initial_sidebar_state=PAGE_INITIAL_STATE,
     )
     st.header(APP_TITLE, divider='rainbow')
-    
+
     # Não remover ou alterar esta seção, a menos que saiba exatamente o que está fazendo.
     if 'files' not in st.session_state:
-        # Carrega files.tsv no st.session_state.files. 
+        # Carrega files.tsv no st.session_state.files.
         # ESSENCIAL para o funcionamento dos caminhos de arquivo.
         st.session_state['files'] = {}
+        project_root = os.getcwd() # Obtém o diretório raiz do projeto
         with open('files.tsv', 'r', encoding='utf-8') as file:
             files_paths = [name_path.split('\t') for name_path in file.read().split('\n') if name_path.strip()][1:]
             for name, path in files_paths:
-                st.session_state['files'][name] = path
+                # Converte o caminho relativo em absoluto
+                st.session_state['files'][name] = os.path.join(project_root, path)
 
     # Cria o menu de navegação. ESSENCIAL para a estrutura da aplicação.
     from menu import create_menu  # deve ser importado só após a criação de 'files' na session_state
